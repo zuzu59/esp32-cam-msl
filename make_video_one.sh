dad092690050422a8a48ac506e63501c1b297172
Christian Zufferey <christian@zufferey.com>
2023-11-14 15:48:46 +0100

    change: Diminué le débit de la vidéo à 1M seconde

--- make_video_one.sh
+++ make_video_one.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Petit script pour la réalisation d'un petit film à base d'images pour tous les espaces du MsL
-# zf231114.1125
+# zf231114.1530
 #
 # Sources: https://www.rickmakes.com/create-timelapse-from-ip-camera-using-curl-wget-and-ffmpeg/
 
@@ -21,6 +21,7 @@ Dossier pour les images: $1
 Dossier pour les vidéos: $2
 "
 
-ffmpeg -framerate 8 -pattern_type glob -i "$1/*.jpg" -vf crop=in_w:in_w*3/4,scale=1600:1200 -c:v libx264 -b:v 10000k -r 30 -pix_fmt yuv420p $2/0_video.mp4
+#ffmpeg -framerate 8 -pattern_type glob -i "$1/*.jpg" -vf crop=in_w:in_w*3/4,scale=1600:1200 -c:v libx264 -b:v 10000k -r 30 -pix_fmt yuv420p $2/0_video.mp4
+ffmpeg -framerate 8 -pattern_type glob -i "$1/*.jpg" -vf crop=in_w:in_w*3/4,scale=1600:1200 -c:v libx264 -b:v 1000k -r 30 -pix_fmt yuv420p $2/0_video.mp4
 
 
