8ff3adafe586488a361d67be17c902e757ca1315
Christian Zufferey <christian@zufferey.com>
2023-11-14 14:31:05 +0100

    change: rendu non dépendant du dossier où tournent les scripts pour le crontab

--- capture_all.sh
+++ capture_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Petit script pour prendre une image de toutes les mini caméras esp32-cam
-#zf231114.1059
+#zf231114.1430
 
 
-
-zDATA_FOLDER=./data
+zAPP_FOLDER=$(/usr/bin/dirname $0)
+zDATA_FOLDER=$zAPP_FOLDER/data
 zIMAGES_FOLDER=$zDATA_FOLDER/images
 zACTUAL_FOLDER=$zDATA_FOLDER/actual
 
@@ -18,5 +18,15 @@ zTARGET=$zIMAGES_FOLDER/$zYEAR/$zMONTH/$zDAY
 echo -e $zTARGET
 
 zESPACE=bois
-./capture_one.sh $zTARGET/$zESPACE 192.168.8.60 $zTIME.jpg
+$zAPP_FOLDER/capture_one.sh $zTARGET/$zESPACE 192.168.8.60 $zTIME.jpg
 cp $zTARGET/$zESPACE/$zTIME.jpg $zACTUAL_FOLDER/$zESPACE.jpg
+
+echo -e "
+
+Si jamais pour info:
+
+crontab -e
+*/1 * * * * /home/ubuntu/dev/esp32-cam-msl/capture_all.sh  (capture une image toutes les 1 minutes)
+
+"
+
--- make_video_all.sh
+++ make_video_all.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Petit script pour la réalisation d'un petit film à base d'images pour tous les espaces du MsL
-# zf231114.1110
+# zf231114.1430
 #
 # Sources: https://www.rickmakes.com/create-timelapse-from-ip-camera-using-curl-wget-and-ffmpeg/
 
-zDATA_FOLDER=./data
+zAPP_FOLDER=$(/usr/bin/dirname $0)
+zDATA_FOLDER=$zAPP_FOLDER/data
 zIMAGES_FOLDER=$zDATA_FOLDER/images
 zACTUAL_FOLDER=$zDATA_FOLDER/actual
 zVIDEOS_FOLDER=$zDATA_FOLDER/videos
@@ -23,6 +24,16 @@ echo -e $zTARGET_VIDEOS
 zESPACE="bois"
 mkdir -p $zTARGET_VIDEOS/$zESPACE
 rm $zTARGET_VIDEOS/$zESPACE/0_video.mp4
-./make_video_one.sh $zTARGET_IMAGES/$zESPACE $zTARGET_VIDEOS/$zESPACE
+$zAPP_FOLDER/make_video_one.sh $zTARGET_IMAGES/$zESPACE $zTARGET_VIDEOS/$zESPACE
 cp $zTARGET_VIDEOS/$zESPACE/0_video.mp4 $zACTUAL_FOLDER/$zESPACE.mp4
 
+
+
+echo -e "
+
+Si jamais pour info:
+
+crontab -e
+*/5 * * * * /home/ubuntu/dev/esp32-cam-msl/make_video_all.sh  (réalise une vidéo toutes les 5 minutes)
+
+"
