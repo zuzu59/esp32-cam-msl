b0bc936d52a676d80830f2e4b184583469ea7e5c
root <root@camera-srv.zuzu59.ch>
2024-09-28 16:52:56 +0200

    new: ajouté l'image no video en cas de pannes de la caméra

--- capture_one.sh
+++ capture_one.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Petit script qui ne fait que de prendre une image d'une mini caméra esp32-cam et lui ajoute un timestamp en haut à gauche
-# zf231114.1053, zf240928.1632
+# zf231114.1053, zf240928.1644
 
 #test si l'argument est vide
 if [ -z "$1" ]
@@ -21,12 +21,15 @@ Nom de l'image: $3
 "
 
 
+
+zAPP_FOLDER=$(/usr/bin/dirname $0)
+file_path=$1/$3
+
 mkdir -p $1
 # Récupère l'image de la caméra
+cp $zAPP_FOLDER/no_video-512.jpg $file_path		# pour voir si la caméra est en panne ?  zf240728.1637
 wget -O $1/$3 http://$2/1280x1024.jpg
 
-# Récupère le nom de l'image complet
-file_path=$1/$3
 
 # Extraire le timestamp du nom du fichier
 timestamp=$(basename "$file_path" | cut -d'.' -f1)
