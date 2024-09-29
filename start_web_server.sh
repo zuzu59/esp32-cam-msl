bbbd25e821dc9180fe05014ba745b71f62d19c10
root <root@camera-srv.zuzu59.ch>
2024-09-29 15:45:09 +0200

    wip: j'essaie d'être responsiv design avec le directory browsing, mais pour l'instant, ce n'est pas le pied

--- start_web_server.sh
+++ start_web_server.sh
@@ -4,8 +4,19 @@
 #
 # ATTENTION: il faut modifier le port et le dossier racine dans le fichier $zAPP_FOLDER/lighttpd.conf !
 #
-#zf231211.1756, zf240731.1556, zf240928.1717
-
+#zf231211.1756, zf240731.1556, zf240929.1544
+#
+# Remarques:
+# Pour que le directory browsing soit responsive sur un smartphone, il faut modifier le CSS du directory-browsing !
+# C'est pourquoi il faut copier le fichier custom.css dans la racine du site
+#
+# Pour avoir un joli dashboard, il faut copier le fichier index.html dans la racine
+# Mais attention, si le site WEB est en direct sur Internet, tout le monde pourra le voir.
+# Il faut donc le protéger derrière ClourdFlared avec un mot de pass !
+#
+# Sources:
+# https://redmine.lighttpd.net/projects/lighttpd/wiki/Mod_dirlisting#dir-listingexternal-css
+#
 
 zAPP_FOLDER=$(/usr/bin/dirname $0)
 zDATA_FOLDER=/mnt/data
@@ -20,7 +31,13 @@ sudo pkill lighttpd
 
 # permet de rendre non fonctionnel le directory browsing dans la racine
 # afin de diminuer la surface d'attaque
-touch $zDATA_FOLDER/index.html
+#touch $zDATA_FOLDER/index.html
+
+# permet d'avoir une page dashboard dans la racine du site
+cp $zAPP_FOLDER/index.html $zDATA_FOLDER/index.html
+
+# permet d'être responsive sur un smartphone
+cp $zAPP_FOLDER/custom.css $zDATA_FOLDER/custom.css
 
 sudo lighttpd -f $zAPP_FOLDER/lighttpd.conf
 
@@ -31,17 +48,14 @@ Si jamais pour info:
 
 crontab -e
 @reboot  sleep 30 && /root/dev/esp32-cam-st-luc/start_web_server.sh     # Démarre le serveur web au boot de la machine
-Et pour l'arrêter facilement il faut faire: 
+
+Et pour l'arrêter, il faut faire: 
 
 sudo pkill lighttpd
 
+Pour le redémarer, il faut faire:
+
+sudo pkill lighttpd ; ./start_web_server.sh
+
+
 "
-
-
-exit
-
-
-while true ; do
-    pushd $zDATA_FOLDER; python3 -m http.server 30080; popd
-done
-
